1cea8f01364acdf16e60363714b268efb9f10728
cfsen <103598332+cfsen@users.noreply.github.com>
2025-09-05 21:37:09 -0400

    chore: correct indenting

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,26 @@ import Page from './components/Layout/page'
 import './App.css'
 
 function App() {
-  const [displayPage, setDisplayPage] = useState(0);
+	const [displayPage, setDisplayPage] = useState(0);
 
-  const handleNavigation = (navTo: number) => {
-    setDisplayPage(navTo);
-  }
+	const handleNavigation = (navTo: number) => {
+		setDisplayPage(navTo);
+	}
 
-  return (
-    <>
-      <div id="mainContainer">
-        <h1>cfsen</h1>
-        <i>Developer, electronics engineer.</i>
+	return (
+		<>
+			<div id="mainContainer">
+				<h1>cfsen</h1>
 
-        <Navigation activePage={displayPage} handleActive={handleNavigation} />
-        <Page activePage={displayPage} handleActive={handleNavigation} />
+				<Navigation activePage={displayPage} handleActive={handleNavigation} />
+				<Page activePage={displayPage} handleActive={handleNavigation} />
 
-      </div>
-      <p className="read-the-docs">
-        Want to check out the source code for this portfolio? <a href="#" target="_blank">Here's the Github repository.</a>
-      </p>
-    </>
-  )
+			</div>
+			<p className="read-the-docs">
+				Want to <a href="#" target="_blank">check out the source code</a>?
+			</p>
+		</>
+	)
 }
 
 export default App
